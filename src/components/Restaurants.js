21f0f83c697ada3df43f2bfc4a6e4d2d119ea3d9
Laurice Parkin <lauriceparkin@lps-MacBook-Pro-2.local>
2020-02-28 10:37:20 -0500

    merge

---
+++ src/components/Restaurants.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import '../App.css';
+
+function Restaurants(props) {
+  return (
+    <div className="result">
+      <p className="rest">{props.eatery.dba}</p>
+      <p>{props.eatery.building} {props.eatery.street}</p>
+      <p>{props.eatery.cuisine_description}</p>
+      <p>{props.eatery.grade}</p>
+    </div>
+  )
+}
+
+export default Restaurants;
+
+
+
